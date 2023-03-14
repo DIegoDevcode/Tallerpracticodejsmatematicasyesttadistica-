74ea8a1bd25ca5bb8545facf4a212886617118fb
DIegoDevcode <jdiegomoreno05@gmail.com>
2023-03-14 16:14:08 -0500

    Añadiendo el metodo find al ejercicio

--- js/descuentos.js
+++ js/descuentos.js
@@ -6,6 +6,29 @@ const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
+
+//const couponObj = {
+//    'JuanDC_es_Batmat': 30,
+//    '3456789': 25,
+//    '123': 15,
+//};
+
+const couponsList = [];
+couponsList.push({
+  name: 'JuanDC_es_Batman',
+  discount: 30,
+});
+couponsList.push({
+  name: 'pero_es_un_secreto',
+  discount: 25,
+});
+couponsList.push({
+  name: 'no_le_digas_a_nadie',
+  discount: 15,
+});
+
+
+
 function calcularPrecioConDescuento(){
     const price=Number(inputPrice.value);
     const Coupon=inputCoupon.value;
@@ -15,19 +38,34 @@ function calcularPrecioConDescuento(){
         return;
     }
 
-let discount;
+    let discount;
 
-    switch (Coupon) {
-    case 'JuanDC_es_Batman':
-        discount = 30;
-        break;
-    case 'no_le_digas_a_nadie':
-        discount = 25;
-        break;
-    default:
-        pResult.innerText = 'El cupón no es válido';
-        return;
-}  
+    function isCouponInArray(couponElement) {// {name, discount}
+      return couponElement.name == Coupon;
+    }
+    
+    const couponInArray = couponsList.find(isCouponInArray); // {}
+  
+    if (couponInArray) {
+      discount = couponInArray.discount;
+    } else {
+      pResult.innerText = 'El cupón no es válido';
+      return;
+    }
+  
+    console.log({
+      Coupon, 
+      discount,
+      couponInArray,
+      couponsList,
+    });
+    // if(couponObj[Coupon]){
+    //    discount = couponObj[Coupon]
+    //} else{
+    //pResult.innerText = 'El cupón no es válido';
+    //return;
+    //}
+
   // if (coupon == 'JuanDC_es_Batman') {
   //   discount = 30;
   // } else if (coupon == 'no_le_digas_a_nadie') {
